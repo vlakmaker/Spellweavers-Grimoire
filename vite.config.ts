0870d3afb2763cb6407544fb80c8e5e7a7658c77
Vera Lakmaker <vera.lakmaker@gmail.com>
2025-04-07 20:49:49 +0200

    updated docker-compose and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
-    port: 3000
-  }
-});
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['grimoire.bittygpt.com'],
+  },
+})
